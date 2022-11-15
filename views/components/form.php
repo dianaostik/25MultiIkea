58eb79a3916b7a7dab22c40e715a7e3efaeaa3d6
dianaostik <dianastud12@gmail.com>
2022-11-15 15:59:41 +0200

    with validator in form

--- views/components/form.php
+++ views/components/form.php
@@ -1,5 +1,11 @@
 <?php 
-include $_INNER_PATH ."/routes.php"; 
+include $_INNER_PATH ."/routes.php";
+$old = false;
+if(isset($_SESSION['POST'])){
+    if(count($_SESSION['POST']) != 0){
+        $old = true;
+    }
+}
 ?>
 
 <body style="background-color: #F2F4F7">
@@ -7,22 +13,24 @@ include $_INNER_PATH ."/routes.php";
 
 <div class="form-group">
     <label for="f1">Prekės pavadinimas:</label>
-    <input type="text"  style="background-color: #F9FBFC" name="name" id="f1" class="form-control" value="<?= ($edit)? $item->name : "" ?>">
+    <input type="text"  style="background-color: #F9FBFC" name="name" id="f1" class="form-control" value="<?=($old)? $_SESSION['POST']['name'] : ""?>">
 </div>
 <div class="form-group">
     <label for="f2">Kategorija:</label>
-    <input type="text" style="background-color: #F9FBFC" name="category" id="f2" class="form-control"  value="<?= ($edit)? $item->category : "" ?>">
+    <input type="text" style="background-color: #F9FBFC" name="category" id="f2" class="form-control"  value="<?=($old)? $_SESSION['POST']['category'] : ""?>">
 </div>
 <div class="form-group">
     <label for="f3">Kaina:</label>
-    <input type="number" step=".01" style="background-color: #F9FBFC" name="price" id="f3" class="form-control"  value="<?= ($edit)? $item->price : "" ?>">
+    <input type="number" step=".01" style="background-color: #F9FBFC" name="price" id="f3" class="form-control"  value="<?=($old)? $_SESSION['POST']['price'] : ""?>">
 </div>
 <div class="form-group">
     <label for="f4">Prekės aprašymas:</label>
-    <textarea name="about" cols="40" rows="3" id="f4" class="form-control" > <?= ($edit)? $item->about : "" ?> </textarea>
+    <textarea name="about" cols="40" rows="3" id="f4" class="form-control" > <?=($old)? $_SESSION['POST']['about'] : ""?> </textarea>
 </div>
     <button type="submit" name="save" class="btn btn-outline-primary mt-3 mb-3">Išsaugoti</button>
 </body>
 </html>
 
-
+<?php
+    $_SESSION['POST'] = [];
+?>
